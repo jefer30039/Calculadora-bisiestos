390311221b3c689818db649b7f348ebe26a92858
jefer30039 <jefer30039@gmail.com>
2024-10-14 09:04:10 -0600

    ajuste del cuadro de entrada y boton

--- interfaz.c
+++ interfaz.c
@@ -67,14 +67,14 @@ int main (int argc, char *argv[])
     gtk_entry_set_max_length(GTK_ENTRY(entry), 4);
     gtk_entry_set_width_chars(GTK_ENTRY(entry), 4);
     gtk_widget_set_halign(entry, GTK_ALIGN_CENTER);
-    gtk_widget_set_valign(entry, GTK_ALIGN_CENTER);
-    gtk_widget_set_size_request(entry, 100, 30);
+    gtk_widget_set_valign(entry, GTK_ALIGN_END);
+    gtk_widget_set_size_request(entry, 150, 30);
 
     //boton para calcular si el año es bisiesto
     button = gtk_button_new_with_label("Calcular");
-    gtk_widget_set_size_request(button, 100, 30); // Ajusta el tamaño del botón
+    gtk_widget_set_size_request(button, 150, 30); // Ajusta el tamaño del botón
     gtk_widget_set_halign(button, GTK_ALIGN_CENTER); // Alinea el botón al centro horizontalmente
-    gtk_widget_set_valign(button, GTK_ALIGN_CENTER); // Alinea el botón al centro verticalmente
+    gtk_widget_set_valign(button, GTK_ALIGN_START); // Alinea el botón al centro verticalmente
     g_signal_connect(button, "clicked", G_CALLBACK(mostrar_resultado), entry);
 
     //contenedor para los elementos
@@ -82,8 +82,8 @@ int main (int argc, char *argv[])
     vbox = gtk_box_new(FALSE, 5);
 
     gtk_box_pack_start(GTK_BOX(vbox), label, TRUE, TRUE, 0);
-    gtk_box_pack_start(GTK_BOX(hbox), entry, TRUE, TRUE, 0);
-    gtk_box_pack_start(GTK_BOX(hbox), button, TRUE, TRUE, 0);
+    gtk_box_pack_start(GTK_BOX(hbox), entry, TRUE, TRUE, 7);
+    gtk_box_pack_start(GTK_BOX(hbox), button, TRUE, TRUE, 7); 
     gtk_box_pack_start(GTK_BOX(vbox), hbox, TRUE, TRUE, 0);
 
     gtk_container_add(GTK_CONTAINER(window), vbox);
